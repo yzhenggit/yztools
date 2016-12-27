a0ae23732ee6290c24f411262062994750d0511e
yhzheng <yz2505@columbia.edu>
2016-12-27 11:13:43 -0500

    Add a 3D function

--- bin_spec.py
+++ bin_spec.py
@@ -7,7 +7,19 @@ def bin_spec(x1, y1, bins):
     b1 = np.mgrid[0:len(x1):bins]
     dg1 = np.digitize(np.mgrid[0:len(x1):1], b1)
 
-    dg_x1 = np.array([np.mean(x1[dg1==j]) for j in range(len(b1)+1)])
-    dg_y1 = np.array([np.mean(y1[dg1==j]) for j in range(len(b1)+1)])
+    dg_x1 = np.array([np.mean(x1[dg1==j]) for j in np.arange(len(b1)+1)[1:]])
+    dg_y1 = np.array([np.mean(y1[dg1==j]) for j in np.arange(len(b1)+1)[1:]])
+    
     return dg_x1, dg_y1
 
+def bin_spec_3Dcube(x1, y1, bins, ax=0):
+    
+    import numpy as np
+
+    b1 = np.mgrid[0:len(x1):bins]
+    dg1 = np.digitize(np.mgrid[0:len(x1):1], b1)
+
+    dg_x1 = np.array([np.mean(x1[dg1==j], axis=ax) for j in np.arange(len(b1)+1)[1:]])
+    dg_y1 = np.array([np.mean(y1[dg1==j], axis=ax) for j in np.arange(len(b1)+1)[1:]])
+
+    return dg_x1, dg_y1
