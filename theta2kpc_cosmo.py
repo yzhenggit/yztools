9f2eb12a2859b93e9b95d21f84140d169e10d73e
yhzheng <yz2505@columbia.edu>
2021-03-05 08:32:08 -0800

    add options for conversion at z or distance

--- theta2kpc_cosmo.py
+++ theta2kpc_cosmo.py
@@ -1,8 +1,12 @@
 #def deg2mpc_input_dL(theta_deg, dA_mpc, do_print=False):
 
 #def deg2mpc_input_dA(theta_deg, dA_mpc, do_print=False):
-
-def arcmin2mpc_input_z(theta_arcmin, redshift, do_print=False):
+# example:
+# for redshfit
+# $ python theta2kpc_cosmo.py 7.12 arcmin 0.1 z 
+# for distance
+# $ python theta2kpc_cosmo.py 7.12 arcmin 48.2 kpc
+def arcmin2kpc_input_z(theta_arcmin, redshift, do_print=False):
     """
     Calculation the linear size (mpc) of a subject wtih angular size of
     theta at redshift z
@@ -12,7 +16,7 @@ def arcmin2mpc_input_z(theta_arcmin, redshift, do_print=False):
             angular diameter distance d_A in Mpc
 
     Example:
-    command line: $python theta2mpc_cosmo.py 33 arcsec z 0.2
+    $ python theta2kpc_cosmo.py 7.12 arcmin 48.2 kpc
     """
     # impact and dist both in kpc
     from astropy.cosmology import Planck15 as cosmo
@@ -40,7 +44,36 @@ def arcmin2mpc_input_z(theta_arcmin, redshift, do_print=False):
               'input_z': redshift,
               'dL_mpc': dL,
               'dA_mpc': dA}
-    return result 
+    return result
+
+def arcsec2kpc_local(theta_arcsec, dist_kpc, do_print=True):
+    """
+    Input
+    theta_arcsec: angular size of object in unit of arcsec
+    dist_kpc: distance to the target in unit of kpc
+
+    output:
+    size_kpc
+
+    Example:
+    $ python theta2kpc_local.py 7.12 arcmin 48.2 kpc
+    """
+
+    size_kpc = dist_kpc/206265 * theta_arcsec
+    size_pc = size_kpc*1000
+
+    if do_print == True:
+        print('*'*60)
+        print('Input: ')
+        print('    theta=%.2f arcmin, %.1f arcsec'%(theta_arcsec/60, theta_arcsec))
+        print('    distance = %.1f kpc'%(dist_kpc))
+        print('Output: ')
+        print('    %.2f arcsec = %.5f kpc / %.2f pc'%(theta_arcsec, size_kpc, size_pc))
+
+    result = {'theta_arcsec':theta_arcsec,
+              'size_kpc': size_kpc,
+              'input_d_kpc': dist_kpc}
+    return result
 
 if __name__ == "__main__":
     import sys
@@ -57,8 +90,20 @@ if __name__ == "__main__":
         print('Do not recognize theta_unit %s.'%(theta_unit))
         sys.exit()
 
-    dist_note = sys.argv[3]
-    dist = np.float(sys.argv[4])
-    if dist_note.lower() in ['z' ,'redshift']:
+    # then check the distance input
+    dist = np.float(sys.argv[3])
+    dist_unit = sys.argv[4]
+    if dist_unit.lower() in ['kpc']:
+        dist_kpc = dist
+        theta_arcsec = theta_arcmin*60
+        res = arcsec2kpc_local(theta_arcsec, dist_kpc, do_print=True)
+    elif dist_unit.lower() in ['mpc']:
+        dist_kpc = dist*1000
+        theta_arcsec = theta_arcmin*60
+        res = arcsec2kpc_local(theta_arcsec, dist_kpc, do_print=True)
+    elif dist_unit.lower() in['z', 'redshift']:
         redshift = dist
-        res = arcmin2mpc_input_z(theta_arcmin, redshift, do_print=True)
+        res = arcmin2kpc_input_z(theta_arcmin, redshift, do_print=True)
+    else:
+        print("Only know units of kpc/Kpc/MPC, mpc/Mpc/MPC, or z/redshift.")
+        sys.exit()
