b417b56d9de1184f53c5698be8d94f195b53ee4f
yhzheng <yz2505@columbia.edu>
2021-03-23 14:45:17 -0700

    add info about version

--- search_milliquas_galex_catalog.py
+++ search_milliquas_galex_catalog.py
@@ -1,9 +1,13 @@
-def search_milliquas_galex_catalog(gal_name, gal_ra, gal_dec,
-                                   gal_dist_kpc, within_radius_kpc=100,
-                                   fuvmag_limit=18.5):
+def search_milliquas_galex_catalog(gal_name, gal_coord1, gal_coord2,
+                               gal_dist_kpc,
+                               within_radius_kpc=100,
+                               frame='icrs', fuvmag_limit=18.5):
     """
-    Search new QSOs within certain radius of (gal_ra, gal_dec)
+    Search new QSOs within certain radius of (gal_coord1, gal_coord2)
     from the cross-matched Million QSO catalog (Flesch 2017, v5.2) and Galex.
+
+    when frame=icrs, gal_coord1 = ra, gal_coord2 = dec, in unit of deg
+    when frame = galactic, gal_coord1 = gl, gal_coord_coord2 = gb, in unit of deg
 
     gal_ra: ra of host galaxy, in unit of degree
     gal_dec: dec of host galaxy, in unit of degree
@@ -16,16 +20,24 @@ def search_milliquas_galex_catalog(gal_name, gal_ra, gal_dec,
     from astropy.coordinates import SkyCoord
     from astroquery import mast
     from astropy.table import Table
-    from yzObs.kpc2deg import kpc2deg
+    from yztools.kpc2deg import kpc2deg
 
     # coordinate setup for central galaxy
-    gal_coord = SkyCoord(ra=gal_ra*u.deg, dec=gal_dec*u.deg,
-                         frame='icrs', distance=gal_dist_kpc*u.kpc)
+    if frame.lower() == 'icrs':
+        gal_coord = SkyCoord(ra=gal_coord1*u.deg, dec=gal_coord2*u.deg, frame='icrs', distance=gal_dist_kpc*u.kpc)
+    elif frame.lower() in ['galactic', 'gal', 'g']:
+        gal_coord = SkyCoord(l=gal_coord1*u.deg, b=gal_coord2*u.deg, frame='galactic', distance=gal_dist_kpc*u.kpc)
+    else:
+        print("Do no recognize frame:%d. "%(frame))
+        sys.exit(0)
 
+    gal_ra = gal_coord.icrs.ra.degree
+    gal_dec = gal_coord.icrs.dec.degree
     print("*"*90)
     print("Cross match the Million QSOs (Fleshch17; v5.2) + GALEX catalog")
     print("within %.1f kpc of %s (RA=%.4f, DEC=%.4f, l=%.4f, b=%.4f)"%(within_radius_kpc,
-           gal_name, gal_ra, gal_dec, gal_coord.galactic.l.degree, gal_coord.galactic.b.degree))
+                           gal_name, gal_coord.icrs.ra.degree, gal_coord.icrs.dec.degree,
+                           gal_coord.galactic.l.degree, gal_coord.galactic.b.degree))
 
     # read in the QSO catalog
     cat_dir = '/Users/Yong/Dropbox/Databucket'
@@ -85,3 +97,19 @@ def search_milliquas_galex_catalog(gal_name, gal_ra, gal_dec,
             print('%25s  %8.3f  %5.2f  %10.4f  %10.4f  %10.4f  %10.4f  %7.1f(%.2f)'%(c1[k],
                   c6[k], c8[k], c2[k], c3[k], c4[k], c5[k], c10[k], c11[k]))
     print("\n")
+
+if __name__ == '__main__':
+    import sys
+    import numpy as np
+
+    gal_name = sys.argv[1]
+    frame = sys.argv[2]  # 'icrs' or 'galactic'
+    gal_coord1 = np.float(sys.argv[3]) # deg, ra, or gl
+    gal_coord2 = np.float(sys.argv[4]) # deg, ra, or gb
+    gal_dist_kpc = np.float(sys.argv[5])  # kpc
+    within_radius_kpc = np.float(sys.argv[6]) # kpc
+
+    search_milliquas_galex_catalog(gal_name, gal_coord1, gal_coord2,
+                                   gal_dist_kpc,
+                                   within_radius_kpc=within_radius_kpc,
+                                   frame=frame)
