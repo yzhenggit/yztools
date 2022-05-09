0136be7162ff80c903ee45a44e8b2de60d24a73b
yhzheng <yz2505@columbia.edu>
2022-05-09 12:08:32 -0700

    updated smhm with Behroozi+2013 and Garrison-Kimmel+2017

---
+++ mhalo2mstar.py
@@ -0,0 +1,152 @@
+import numpy as np
+
+def mhalo2mstar_gk14(log_mhalo, do_print=False):
+    """Garrison-Kimmel+2014a halo abundance matching for dwarfs logM*=6-8 """
+    import numpy as np
+
+    ## from Garrison-Kimmel+2014a, eq4
+    # Mhalo = 10**np.arange(6, 17,0.001)
+    alpha = 1.92
+    mstar = 3e6*(10**log_mhalo/1e10)**alpha
+
+    if do_print == True:
+        print(">> Note that Garrison-Kimmel+2014a derivation is for logM*=6-8, FIRE use Kroupa IMF\n")
+
+    return np.log10(mstar)
+
+def mhalo2mstar_moster13(log_mhalo, do_print=False):
+    """
+    Moster+2013's halo abundance matching.
+    Based on Chabrier IMF
+    Calculated over stellar mass of logM*=7.4--11.7 Msun.
+
+    History:
+    YZ, 05/06/2022, UCB
+    """
+
+    import astropy.constants as const
+    from astropy.cosmology import Planck15
+    import astropy.units as u
+    import numpy as np
+
+    # from Table 1 in Moster+2013, 0 mean z=0
+    log_M10 = 11.59
+    M10 = 10**log_M10 # Msun,
+    sig_logM10 = 0.236
+    sig_M10 = sig_logM10*M10*np.log(10)
+
+    N10 = 0.0351
+    sig_N10 = 0.0058
+
+    beta10 = 1.376
+    sig_beta10 = 0.153
+
+    gamma10 = 0.608
+    sig_gamma10 = 0.059
+
+    # from moster equation 2
+    mhalo = 10**log_mhalo
+    mstar = mhalo * (2.*N10*((mhalo/M10)**(-beta10)+(mhalo/M10)**(gamma10))**(-1.))
+
+    if do_print == True:
+        print(">> Note that Moster+2013 derivation is for logM*=7.4-11.7, Chabrier IMF")
+    return np.log10(mstar)
+
+def mhalo2mstar_moster10(log_mhalo, do_print=False):
+    """
+    Moster+2010's halo abundance matching. Based on Joo's code.
+    Based on Kroupa 2001 IMF
+    Calculated over stellar mass of logM*=8.5--11.85 Msun.
+    """
+    import numpy as np
+
+    # from Table 6 in Moster+2010
+    logM1 = 11.88
+    sig_logM1 = 0.02
+    M1 = 10**logM1 # Msun
+    sig_M1 = sig_logM1*M1*np.log(10)
+
+    mM0 = 0.0282
+    sig_mM0 = 0.0005
+
+    beta = 1.06
+    sig_beta = 0.05
+
+    gamma = 0.56
+    sig_gamma = 0.0
+
+    # from moster equation 2
+    mhalo = 10**log_mhalo
+    mstar = mhalo * (2.*mM0*((mhalo/M1)**(-beta)+(mhalo/M1)**(gamma))**(-1.))
+
+    if do_print == True:
+        print(">> Note that Moster+2010 derivation is for logM*=8.5-11.85, Kroupa IMF\n")
+    return np.log10(mstar)
+
+def b13_fx(x, alpha, delta, gamma):
+    # Eq 3 in Behroozi+2013
+    # f(x)
+    import numpy as np
+    parta = -np.log10(10**(alpha*x)+1)
+    partb = delta*(np.log10(1+np.exp(x)))**gamma / (1+np.exp(10**-x))
+
+    return parta + partb
+
+def mhalo2mstar_behroozi13(log_mhalo, do_print=False):
+    Mhalo = 10**log_mhalo
+
+    # intrinsic parameter values as listed in Sec. 5 in Behroozi+2013
+    log10_epsilon0 = -1.777
+    epsilon0 = 10**log10_epsilon0
+
+    log10_M10 = 11.514
+    M10 = 10**log10_M10
+
+    alpha0 = -1.412
+    delta0 = 3.508
+    gamma0 = 0.316
+
+    # Eq 3
+    x = np.log10(Mhalo/M10)
+    log10_Mstar = np.log10(epsilon0*M10) + b13_fx(x, alpha0, delta0, gamma0) - b13_fx(0, alpha0, delta0, gamma0)
+
+    if do_print == True:
+        print(">> Note that Behroozi+2013 derivation is for logM*=7.25-11.85, Chabrier IMF\n")
+    return log10_Mstar
+
+def mhalo2mstar_behroozi13_lowmass_powerlaw(log_mhalo):
+    # only for lower mass end
+    Mhalo = 10**log_mhalo
+
+    log10_M10 = 11.514
+    M10 = 10**log10_M10
+
+    alpha0 = -1.412
+
+    x = np.log10(Mhalo/M10)
+    # log10_Mstar = np.log10(epsilon0*M10) + b13_fx(x, alpha0, delta0, gamma0) - b13_fx(0, alpha0, delta0, gamma0)
+    # 9.392 = np.log10(epsilon0*M10) - b13_fx(0, alpha0, delta0, gamma0)
+    log10_Mstar = 9.392 - alpha0*x
+
+    return log10_Mstar
+
+def mhalo2mstar_gk17(log_mhalo, scatter=0.2, do_print=False):
+    Mhalo = 10**log_mhalo
+
+    log10_M10 = 11.514
+    M10 = 10**log10_M10
+
+    # assuming constant scatter, field dwarfs, Eq 6 in Garrison-Kimmel+2017
+    alpha_LF = 0.24*scatter**2 + 0.16*scatter + 1.99
+
+    x = np.log10(Mhalo/M10)
+    # log10_Mstar = np.log10(epsilon0*M10) + b13_fx(x, alpha0, delta0, gamma0) - b13_fx(0, alpha0, delta0, gamma0)
+    # 9.392 = np.log10(epsilon0*M10) - b13_fx(0, alpha0, delta0, gamma0)
+    # when x=0, doesn't matter what input alpha is in b13_fx
+    log10_Mstar = 9.392 + alpha_LF*x
+
+    if do_print == True:
+        print(">> Note that GK14 derivation is for logM*=6-8, Kroupa IMF in FIRE\n")
+        print(">> this relation matches with Behroozi+2013 at the characteristic mass M1. ")
+
+    return log10_Mstar
--- mstar2mhalo.py
+++ mstar2mhalo.py
@@ -1,37 +1,84 @@
-def mstar2mhalo(mstar, do_print=False):
+from yztools.mhalo2mstar import *
 
-    """
-    Moster+2010's halo abundance matching. Based on Joo's code. 
-    """
+def mstar2mhalo_moster10(log_mstar):
 
-    import astropy.constants as const
-    from astropy.cosmology import Planck15
-    import astropy.units as u
     import numpy as np
-
-    M1 = 10**11.884 # Msun, from Table 4 or 6 in Moster+2010
-    mM0 = 0.0282
-    beta = 1.06
-    gamma = 0.556
-    # Mhalo = 10**np.arange(7,13,0.001)
-    Mhalo = 10**np.arange(6,15,0.001)
-
-    # from moster equation 2
-    Mstar = Mhalo * (2.*mM0*((Mhalo/M1)**(-beta)+(Mhalo/M1)**(gamma))**(-1.))
+    log_mhalo_arr = np.arange(8,15,0.001)
 
     # interpolate this to mstar
-    from scipy import interpolate
-    func = interpolate.interp1d(Mstar, Mhalo)
-    mhalo = func(mstar)
+    log_mstar_arr = mhalo2mstar_moster10(log_mhalo_arr)
 
-    if do_print == True:
-        print(">> logMh = %.2f (%.1e) for logMstar = %.2f (%.1e)"%(np.log10(mhalo), mhalo,
-                                                            np.log10(mstar), mstar))
-        print(">> Note that Moster+2010 derivation is for logM*=8.5-11.85, see their p4\n")
-    return mhalo
+    # interpolate the result
+    from scipy import interpolate
+    func = interpolate.interp1d(log_mstar_arr, log_mhalo_arr)
+    log_mhalo = func(log_mstar)
+
+    return log_mhalo
+
+def mstar2mhalo_moster13(log_mstar):
+
+    import numpy as np
+    log_mhalo_arr = np.arange(8,15,0.001)
+
+    # interpolate this to mstar
+    log_mstar_arr = mhalo2mstar_moster13(log_mhalo_arr)
+
+    # interpolate the result
+    from scipy import interpolate
+    func = interpolate.interp1d(log_mstar_arr, log_mhalo_arr)
+    log_mhalo = func(log_mstar)
+
+    return log_mhalo
+
+def mstar2mhalo_gk14(log_mstar):
+
+    import numpy as np
+    log_mhalo_arr = np.arange(8,15,0.001)
+
+    # interpolate this to mstar
+    log_mstar_arr = mhalo2mstar_gk14(log_mhalo_arr)
+
+    # interpolate the result
+    from scipy import interpolate
+    func = interpolate.interp1d(log_mstar_arr, log_mhalo_arr)
+    log_mhalo = func(log_mstar)
+
+    return log_mhalo
+
+def mstar2mhalo_dwarfcgm(log_mstar, do_print=False):
+    """
+    specifically derived for the Dwarf CGM project.
+    At low mass end with logM*=6-8, we use Garrison-Kimmel+2017
+    At high mass end with logM*>8, we use Behroozi+2013
+
+    YZ. 05/09/2022
+    """
+
+    import numpy as np
+    if log_mstar < 8:
+        print('Using Garrison-Kimmel+2017...good for logMstar=[6,8]')
+        mhalo2mstar_func = mhalo2mstar_gk17
+        log_mhalo_arr = np.arange(9.8, 11,0.001)
+    else:
+        print('Using Behroozi+2013...good for logMstar=[7.25, 11.5]')
+        mhalo2mstar_func = mhalo2mstar_behroozi13
+        log_mhalo_arr = np.arange(10, 15, 0.001)
+
+    log_mstar_arr = mhalo2mstar_func(log_mhalo_arr)
+
+    # interpolate the result
+    from scipy import interpolate
+    func = interpolate.interp1d(log_mstar_arr, log_mhalo_arr)
+    log_mhalo = func(log_mstar)
+
+    if do_print==True:
+        print('Input: logMstar={:.2f}'.format(log_mstar))
+        print('Output: logMhalo={:.2f}'.format(log_mhalo))
+
+    return log_mhalo
 
 if __name__ == "__main__":
     import sys
     import numpy as np
-    mstar = np.float(sys.argv[1])
-    mstar2mhalo(mstar, do_print=True)
+    log_mstar = float(sys.argv[1])
+    mstar2mhalo_dwarfcgm(log_mstar, do_print=True)
