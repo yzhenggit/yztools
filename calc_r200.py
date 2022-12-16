a45a0e7cc610da65027a622877d05097d49c60d2
yhzheng <yz2505@columbia.edu>
2022-12-16 14:39:15 -0500

    add details to specify 200m or 200c

--- calc_r200.py
+++ calc_r200.py
@@ -1,16 +1,45 @@
-def calc_r200(mhalo, do_print=False):
+import astropy.constants as const
+from astropy.cosmology import Planck15
+import astropy.units as u
+import numpy as np
+import sys
+
+def calc_percentile(para_range, ndecimals=2):
+    import numpy as np 
+    percentile = np.percentile(para_range, [16, 50, 84])
+    best_val = np.around(percentile[1], decimals=ndecimals)
+    sig_lo = -np.around(np.diff(percentile)[0], decimals=ndecimals)
+    sig_hi = np.around(np.diff(percentile)[1], decimals=ndecimals)
+    # print(best_val, sig_lo, sig_hi)
+    
+    return best_val, sig_lo, sig_hi
+
+def r200c(mhalo, delta=200): 
+    # calculate r200 with respect to critical density
+    # delta = 200 ## rho/rho_crit=200
+    rho_c = Planck15.critical_density0
+    r200c = ((3*mhalo/(4.*np.pi*delta*rho_c))**(1./3.)).to(u.kpc)
+    return r200c.value
+
+def r200m(mhalo, delta=200): 
+    # calculate r200 with respect to matter density
+    # delta = 200 ## rho/rho_crit=200
+    rho_m = Planck15.critical_density0 * Planck15.Om0
+    r200m = ((3*mhalo/(4.*np.pi*delta*rho_m))**(1./3.)).to(u.kpc)
+    return r200m.value 
+
+def calc_r200(logmhalo, sig_logmhalo=0., label='200m', do_print=False, Ntrials=4000):
 
     """
     Assuming an isothermal sphere. Based on Joo's code.
-    mhalo is linear
+    Note we required log values as input!
+
+    label: need to calrify whehther logmhalo is based on critical density (c) or mean matter density (m)
+
+    Ntrials: default to 4000 mcmc trials to estimate uncertainties 
     """
 
-    import astropy.constants as const
-    from astropy.cosmology import Planck15
-    import astropy.units as u
-    import numpy as np
-    import sys
-
+    # print("calc_r200: Note! requried logmhalo instead mhalo input!")
     #if mstar != 0:
     #    if use_who == 'M10':
     #        from yztools.mstar2mhalo import mstar2mhalo
@@ -20,29 +49,49 @@ def calc_r200(mhalo, do_print=False):
     #        mhalo = mstar2mhalo_dwarfs(mstar, do_print=do_print)
     # print(mhalo)
 
-    mhalo = mhalo*u.Msun
-    delta = 200 ## rho/rho_crit=200
+    if label not in ['200m', '200c']: 
+        print("label must be either 200m or 200c")
+        sys.exit(0)
 
-    # calculate r200 with respect to critical density
-    rho_c = Planck15.critical_density0
-    r200_c = ((3*mhalo/(4.*np.pi*delta*rho_c))**(1./3.)).to(u.kpc)
+    if sig_logmhalo == 0.: 
+        mhalo = 10**logmhalo * u.Msun 
+
+        # calculate r200 with respect to critical density
+        if label == '200c': 
+            best_r200_kpc = np.around(r200c(mhalo), decimals=1)
+        else# label == '200m': 
+        # calculate r200 with respect to matter density
+            best_r200_kpc = np.around(r200m(mhalo), decimals=1)
+        sig_low_r200, sig_hi_r200 = 0., 0.
+
+    else: # with error bars, use MCMC to estimate uncertainties
+        # e.g., https://astrofrog.github.io/py4sci/_static/Practice%20Problem%20-%20Monte-Carlo%20Error%20Propagation%20-%20Sample%20Solution.html
+        logmhalo_range = np.random.normal(logmhalo, sig_logmhalo, Ntrials)
+        r200_range = np.zeros(Ntrials)+np.nan
+        for i in range(Ntrials): 
+            mhalo = 10**(logmhalo_range[i]) * u.Msun
+            if label == '200c': 
+                r200_range[i] = r200c(mhalo)
+            else: 
+                r200_range[i] = r200m(mhalo)
+        
+        best_r200_kpc, sig_low_r200, sig_hi_r200 = calc_percentile(r200_range, ndecimals=1)
 
     if do_print == True:
-        print(">> If use critical density, delta_c=rho/rho_c=200")
-        print(">> r200c = %.2f kpc\n"%(r200_c.value))
+        if label == '200c'
+            print(">> Use critical density, delta_c=rho/rho_c=200")
+            print(">> r200c = {}+{}{} kpc\n".format(best_r200c_kpc, sig_hi_r200c, sig_low_r200c))
+        else: 
+            print(">> Use mean matter density, delta_c=rho/(rho_c*Omega_m)=200")
+            print(">> r200m = {}+{}{} kpc\n".format(best_r200m_kpc, sig_hi_r200m, sig_low_r200m))
 
-    # calculate r200 with respect to matter density
-    rho_m = Planck15.critical_density0 * Planck15.Om0
-    r200_m = ((3*mhalo/(4.*np.pi*delta*rho_m))**(1./3.)).to(u.kpc)
-
-    if do_print == True:
-        print(">> If use critical MATTER density, delta_c=rho/(rho_c*Omega_m)=200")
-        print(">> r200m = %.2f kpc\n"%(r200_m.value))
-
-    return r200_c, r200_m
+    return best_r200_kpc, sig_low_r200, sig_hi_r200
 
 if __name__ == "__main__":
     import sys
     import numpy as np
-    mhalo = np.float(sys.argv[1])
-    calc_r200(mhalo, do_print=True)
+    logmhalo = float(sys.argv[1])
+    sig_logmhalo = float(sys.argv[2])
+    label = sys.argv[3].lower() # either 200c or 200m 
+ 
+    calc_r200(logmhalo, sig_logmhalo=sig_logmhalo, label=label, do_print=True)
